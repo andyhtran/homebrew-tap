e8bcfc0f0f69b6e80247982428e320fe6f93035c
andyhtran <andyhtran92@gmail.com>
2026-05-20 08:56:12 -0400

    miniwhisper 1.5.1

--- Casks/miniwhisper.rb
+++ Casks/miniwhisper.rb
@@ -1,13 +1,12 @@
 cask "miniwhisper" do
-  version "1.5.0"
-  sha256 "3af6d104e8939fc9a8f12a56511d279438742bfb883629e96a7bc66e28ab90a3"
+  version "1.5.1"
+  sha256 "7b1f0294039d728fa6202ddd8f3fad3f4708702571e34287d4a5727b58d30e54"
 
   url "https://github.com/andyhtran/MiniWhisper/releases/download/v#{version}/MiniWhisper-#{version}.zip"
   name "MiniWhisper"
   desc "Voice-to-text transcription from the menu bar"
   homepage "https://github.com/andyhtran/MiniWhisper"
 
-  auto_updates true
   depends_on macos: ">= :sonoma"
 
   app "MiniWhisper.app"
