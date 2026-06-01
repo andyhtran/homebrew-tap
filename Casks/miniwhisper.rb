b53d3275e10db9583e8f1f498bbba40909c0e5fa
andyhtran <andyhtran92@gmail.com>
2026-05-31 23:17:41 -0400

    miniwhisper 1.5.3

--- Casks/miniwhisper.rb
+++ Casks/miniwhisper.rb
@@ -1,6 +1,6 @@
 cask "miniwhisper" do
-  version "1.5.2"
-  sha256 "f88053ee83513d8893503d18f45e96cfddba76d351e3140e1ad3d197b2686395"
+  version "1.5.3"
+  sha256 "4bfab67c7a477d8a12e9af0c5a2952f99e977f9f02c724db4cf488aa06148df3"
 
   url "https://github.com/andyhtran/MiniWhisper/releases/download/v#{version}/MiniWhisper-#{version}.zip"
   name "MiniWhisper"
