295a6b8b04ce24b66354c041e4942b9a6965f67e
andyhtran <76441965+andyhtran@users.noreply.github.com>
2026-06-30 07:41:36 -0400

    miniwhisper 1.7.2

--- Casks/miniwhisper.rb
+++ Casks/miniwhisper.rb
@@ -1,6 +1,6 @@
 cask "miniwhisper" do
-  version "1.7.1"
-  sha256 "445b87db5346388d305db40c842714bafe51ca5af44fd9b635307aed79e0c8dd"
+  version "1.7.2"
+  sha256 "04915c90e66fff465da048c57a2447acb09d46ef632a896d8d262436dc9f7730"
 
   url "https://github.com/andyhtran/MiniWhisper/releases/download/v#{version}/MiniWhisper-#{version}.zip"
   name "MiniWhisper"
